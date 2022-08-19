7ec73f159e39ff27adb18a44ea837ac73716805d
planBRandom <deherrerabrandon@gmail.com>
2022-08-18 23:42:00 -0500

    add addressstill working on the db

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ require('dotenv').config({path: './config/.env'})
 connectDB()
 
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static('public'))node server.js
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
